78b194622e1b2570274da6f3dd8412a4d34257e3
谢俊伟 <getfilebox@gmail.com>
2015-07-21 16:03:53 +0800

    init

---
+++ HXFloatingSelectorView/HXFloatingSelectorView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HXFloatingSelectorView
+//
+//  Created by MacBook on 7/21/15.
+//  Copyright (c) 2015 MacBook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HXFloatingSelectorView/HXFloatingSelectorView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HXFloatingSelectorView
+//
+//  Created by MacBook on 7/21/15.
+//  Copyright (c) 2015 MacBook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
